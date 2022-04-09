dce1782b5d0ca7e58d0861158f3fdcc9ac2a1b6a
Dhruv Joshi <joshi.dd@somaiya.edu>
2022-04-09 17:23:48 +0530

    Project added

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlaceData = async (sw, ne) => {
+    try{
+        const { data: { data }} = await axios.get(URL, {
+            params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,      
+            },
+            headers: {
+                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+                'X-RapidAPI-Key': '0479b7e2f0msh10a9a16ca3b31b7p195d78jsn59ce49f41572'
+            }
+        });
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
